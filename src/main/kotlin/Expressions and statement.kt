93e3ef2ccec4f6c9f0f6af17d681d0b895c7abd7
dyyyywenn <dywenbusiness@gmail.com>
2020-08-27 12:23:56 +0700

    first commit

---
+++ src/main/kotlin/Expressions and statement.kt
@@ -0,0 +1,22 @@
+/*fun main(){
+    val openOffice = 7
+    val now = 8
+    val office = if (now > openOffice) "Office already open" else "office close"
+    print(office)
+
+}*/
+/*fun main() {
+    sum(1 , 1 * 4)
+
+     }
+
+fun sum(value1: Int, value2: Int) = value1 + value2
+*/
+fun main() {
+    val value1 = 10
+    val value2 = 10
+    sum(value1, value2)
+
+     }
+ fun sum(value1: Int, value2: Int) = value1 + value2
+
